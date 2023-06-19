1786ddf25f15466608bed09a3ad71faa159ab502
Rajesh Singh <rajeshsingh624@gmail.com>
2023-06-19 10:22:19 +1000

    1.21

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Tanzu World! v1.0'
+    return 'Hello, Tanzu World! v1.21'
 
 @app.route('/hello/<username>')
 def hello_user(username):
